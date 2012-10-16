b6b75ba953b440ddcfe8d95d2006e4ce6c2a13b5
Seamus Abshere <seamus@abshere.net>
2012-10-15 20:34:18 -0500

    no longer used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,6 @@ end
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
-MiniTest::Unit.runner = MiniTest::SuiteRunner.new
-MiniTest::Unit.runner.reporters << MiniTest::Reporters::SpecReporter.new
 
 require 'active_record_inline_schema'
 
